552df5330981ea61a5aeae2bc660f42454367c21
KaiZhang2020 <78572839+KaiZhang2020@users.noreply.github.com>
2022-05-23 18:19:18 -0700

    setup1

---
+++ banker program/Account.py
@@ -0,0 +1,10 @@
+class Account:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+    def total_val(self):
+
+    def deposit(self, account, amount):
+
+    def withdraw(self, account, amount):
+
---
+++ banker program/BinarySearch.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def insert(self, root, key):
+        if root is None:
+            return Node(key)
+        else:
+            if root.val == key:
+                return root
+            elif root.val < key:
+                root.right = self.insert(root.right, key)
+            else:
+                root.left = self.insert(root.left, key)
+        return root
+
+class BinarySearchTree:
